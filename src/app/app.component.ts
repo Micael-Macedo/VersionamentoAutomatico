fe67b1b63a68fced91314ee332d1339fecf01361
Micael Macedo Alves Barbosa <micael.barbosa@suporterenova.onmicrosoft.com>
2025-03-07 14:07:23 -0300

    feat: insert components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputsComponent } from "./core/components/inputs/inputs.component";
+import { ButtonComponent } from "./core/components/button/button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputsComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
